080bb99985c85e4aa8150a35ff9079a066b7166a
Brian Kidd <brian.kidd.one@gmail.com>
2021-02-23 18:31:09 -0800

    created the collapsible components

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
+
+/********************different ways to write the same algorithm to expand the collapsible container ************************************/ 
+// collapsibles.forEach((item) =>
+//   item.addEventListener("click", () => {
+//     item.classList.toggle("collapsible--expanded");
+//   })
+// );
+
+// for (let i = 0; i < collapsibles.length; i++ ){
+//   const collapsibleItems = collapsibles[i];
+//   collapsibleItems.addEventListener("click", function() {
+//       this.classList.toggle("collapsible--expanded");
+//   }); 
+// }
+
+
+// for (let i = 0; i < collapsibles.length; i++ ){
+//   const collapsibleItems = collapsibles[i];
+//   collapsibleItems.addEventListener("click", () => {
+//       collapsibleItems.classList.toggle("collapsible--expanded");
+//   }); 
+// }
